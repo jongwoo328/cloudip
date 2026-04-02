1d269bd141973224bdd16c84ce3d1007ac5d6bc7
Lee Jongwoo <jongwoo328@gmail.com>
2026-04-02 22:43:44 +0900

    use NewEncoder, NewDecoder

--- util/json.go
+++ util/json.go
@@ -3,28 +3,21 @@ package util
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"os"
 )
 
 func ReadJSON[T any](file *os.File, data *T) error {
-	readData, err := io.ReadAll(file)
-	if err != nil {
-		return fmt.Errorf("failed to read the file: %v", err)
-	}
-	if err := json.Unmarshal(readData, data); err != nil {
-		return fmt.Errorf("failed to unmarshal the data: %v", err)
+	if err := json.NewDecoder(file).Decode(data); err != nil {
+		return fmt.Errorf("failed to decode the data: %v", err)
 	}
 	return nil
 }
 
 func WriteJSON[T any](file *os.File, data *T) error {
-	writeData, err := json.MarshalIndent(data, "", "\t")
-	if err != nil {
-		return fmt.Errorf("failed to marshal the data: %v", err)
-	}
-	if _, err := file.Write(writeData); err != nil {
-		return fmt.Errorf("failed to write the data: %v", err)
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "\t")
+	if err := encoder.Encode(data); err != nil {
+		return fmt.Errorf("failed to encode the data: %v", err)
 	}
 	return nil
 }
