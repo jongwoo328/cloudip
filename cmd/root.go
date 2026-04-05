b4d2729059445b1bfbe29dec5c8e02bf08552c27
Lee Jongwoo <jongwoo328@gmail.com>
2026-04-05 23:45:19 +0900

    add version command

--- cmd/root.go
+++ cmd/root.go
@@ -13,12 +13,16 @@ var rootCmd = &cobra.Command{
 	Use:   common.AppName,
 	Short: fmt.Sprintf("%s is a CLI tool for identifying whether an IP address belongs to a major cloud provider (e.g., AWS, GCP).", common.AppName),
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) > 0 && args[0] == "version" {
-			fmt.Println(Version)
-			return
-		}
 		result := ip.Check(args)
 		printResult(&result)
+	},
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version of cloudip",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(Version)
 	},
 }
 
@@ -27,6 +31,7 @@ func Execute() error {
 }
 
 func init() {
+	rootCmd.AddCommand(versionCmd)
 	rootCmd.Flags().StringVarP(&common.Flags.Format, "format", "f", "text", "Output format (text, table, json)")
 	rootCmd.Flags().BoolVar(&common.Flags.Header, "header", false, "Print header in the output. Only applicable for 'text', 'table' format")
 	rootCmd.Flags().StringVar(&common.Flags.Delimiter, "delimiter", " ", "Delimiter for the output. Only applicable for 'text' format")
